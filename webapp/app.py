76b8275893abd8431497a938cc916019fee86eb2
Zenan Ji <zenanj1@student.unimelb.edu.au>
2021-05-12 18:59:31 +0800

    add more interface

--- webapp/app.py
+++ webapp/app.py
@@ -3,15 +3,13 @@ from flask import render_template
 import time
 import utils
 import data_analysis
+from cloudant.client import CouchDB
 from cloudant.client import Cloudant
 app = Flask(__name__)
 
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
-@app.route('/123')
-def hello_world():
-   return render_template('main.html')
+client = CouchDB('admin', 'data-miner!', url='http://172.26.133.205:5984', connect=True)
 
 @app.route('/')
 def template_test():
@@ -23,32 +21,41 @@ def gettime():
     return time.strftime("%Y{}%m{}%d %X").format('-','-')
 
 #显示当前处理推特数量
-@app.route("/totalTweeters")
-def get_all_number():
-    current_twts = data_analysis.current_twts()
-    return current_twts
+@app.route("/totaltweeters")
+def get_current_twts():
+    currenttwts = data_analysis.current_twts(client)
+    return currenttwts
 
-#地图散点图显示推特分布
-@app.route("/mapshow")
-def get_map_info():
-    return '123'
+# total tweet number each year each city
+@app.route("/tweetperyearcity")
+def get_total_twts():
+    totaltwt = data_analysis.total_twts(client)
+    return totaltwt
 
-#类似于弹幕，滚动推特文本显示
-@app.route("/tweetShow")
-def get_tweeters():
-    return utils.get_tweeters()
+# language distribution each city
+@app.route("/languagedistribution")
+def get_lang_dis():
+    langdis = data_analysis.lang_dis(client)
+    return langdis
 
-#统计分析结果显示
-@app.route("/analysisResult")
-def get_result_info():
-    return '123'
+# average sentiment score each year each city
+@app.route("/sentiment")
+def get_sentiment():
+    sentiscore = data_analysis.senti_score(client)
+    return sentiscore
 
-#推特词云显示
-@app.route('/wordworld')
-def get_hot_words():
-    return ['123','456']
+# time(hour) distribution this month each city (UTC+10)
+@app.route("/timedistribution")
+def get_timedistribution():
+    timedis = data_analysis.time_dis(client)
+    return timedis
 
 
+# top 30 used hashtags this year each city
+@app.route("/tophashtags")
+def get_tophashtags():
+    tophashtags = data_analysis.top_hashtags(client)
+    return tophashtags
 
 if __name__ == '__main__':
     app.run()
