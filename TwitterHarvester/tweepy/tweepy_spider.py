67c0d6c9940bc88083e3de904fdee888f97259c8
happyjj314 <73373764+happyjj314@users.noreply.github.com>
2021-05-21 14:29:23 +0800

    [CHANGE] the NUMBER OF TWEETS

--- TwitterHarvester/tweepy/tweepy_spider.py
+++ TwitterHarvester/tweepy/tweepy_spider.py
@@ -101,7 +101,7 @@ def run_spider():
         coordinates = str(cities_geo[dbname][0])+','+str(cities_geo[dbname][1])+ ',20km'
         result_type = 'recent'
         until_date = '2021-5-10'
-        max_tweets = 10000
+        max_tweets = 1000
 
         tweets = tweepy.Cursor(api.search, geocode=coordinates,result_type=result_type,
                                count=100).items(max_tweets)
